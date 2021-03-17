da6a0af87d6ea31a58a157b7c2f898488d09dd8b
Den107 <deniskrv@gmail.com>
2021-03-18 00:08:59 +0300

    git init + add config, helpers, views

---
+++ src/js/helpers/validate.js
@@ -0,0 +1,15 @@
+const regExpDic = {
+  email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  password: /^[0-9a-zA-Z]{4,}$/
+};
+
+/**
+ * Function validate
+ * @param {HTMLInputElement} el 
+ * @returns {Boolean} Return true if input valid
+ */
+export function validate(el) {
+  const regExpName = el.dataset.required;
+  if (!regExpDic[regExpName]) return true;
+  return regExpDic[regExpName].test(el.value);
+}
